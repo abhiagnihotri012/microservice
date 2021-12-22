ad4e456b58c01331c3842fa4afcf1b936958e8ac
abhiagnihotri012 <abhihackera@gmail.com>
2021-12-22 23:55:03 +0530

    Initial commit to currency exchange service.

---
+++ Currency-Exchange-Service/src/main/java/com/neosoft/microservices/CurrencyExchange.java
@@ -0,0 +1,33 @@
+package com.neosoft.microservices;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "currency_exchange")
+public class CurrencyExchange {
+
+	@Id
+	private Long id;
+	
+	@Column(name = "currency_from")
+	private String from;
+	
+	@Column(name = "currency_to")
+	private String to;
+	
+	private BigDecimal conversionMultiple;
+	
+	private String environment;
+	
+	public CurrencyExchange() {}
+}
